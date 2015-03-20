8141faedb51f19742e6f481b9620628ae60f01b2
Jesse Hull <jessehull@gmail.com>
2015-03-20 15:50:26 -0700

    grunt build

---
+++ public/dist/application.js
@@ -0,0 +1,62 @@
+'use strict';
+// Init the application configuration module for AngularJS application
+var ApplicationConfiguration = function () {
+    // Init module configuration options
+    var applicationModuleName = 'watchslotsserver';
+    var applicationModuleVendorDependencies = [
+        'ngResource',
+        'ui.router',
+        'ui.bootstrap',
+        'ui.utils'
+      ];
+    // Add a new vertical module
+    var registerModule = function (moduleName, dependencies) {
+      // Create angular module
+      angular.module(moduleName, dependencies || []);
+      // Add the module to the AngularJS configuration file
+      angular.module(applicationModuleName).requires.push(moduleName);
+    };
+    return {
+      applicationModuleName: applicationModuleName,
+      applicationModuleVendorDependencies: applicationModuleVendorDependencies,
+      registerModule: registerModule
+    };
+  }();'use strict';
+//Start by defining the main module and adding the module dependencies
+angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
+// Setting HTML5 Location Mode
+angular.module(ApplicationConfiguration.applicationModuleName).config([
+  '$locationProvider',
+  function ($locationProvider) {
+    $locationProvider.hashPrefix('!');
+  }
+]);
+//Then define the init function for starting up the application
+angular.element(document).ready(function () {
+  //Fixing facebook bug with redirect
+  if (window.location.hash === '#_=_')
+    window.location.hash = '#!';
+  //Then init the app
+  angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
+});'use strict';
+// Use Applicaion configuration module to register a new module
+ApplicationConfiguration.registerModule('core');'use strict';
+// Setting up route
+angular.module('core').config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+    // Redirect to home view when route not found
+    $urlRouterProvider.otherwise('/');
+    // Home state routing
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: 'modules/core/views/home.client.view.html'
+    });
+  }
+]);'use strict';
+angular.module('core').controller('HomeController', [
+  '$scope',
+  function ($scope) {
+  }
+]);
